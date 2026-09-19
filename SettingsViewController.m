422b9e9a74d917d4cfbb6763a7f227e9ff344294
Botanium-git <kbtfrdm@gmail.com>
2026-09-20 02:30:27 +0900

    Add SettingsViewController.m

---
+++ SettingsViewController.m
@@ -0,0 +1,32 @@
+#import "SettingsViewController.h"
+#import "LogViewController.h"
+
+@implementation SettingsViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Settings";
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)];
+    [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"cell"];
+}
+- (void)done { [self dismissViewControllerAnimated:YES completion:nil]; }
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView { return 2; }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section { return section == 0 ? 1 : 1; }
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section { return section == 0 ? @"Diagnostics" : @"About"; }
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryNone;
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"Error & Diagnostic Log";
+        cell.imageView.image = [UIImage systemImageNamed:@"exclamationmark.bubble"];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    } else {
+        cell.textLabel.text = @"ScarletX 0.1.1";
+        cell.imageView.image = [UIImage systemImageNamed:@"info.circle"];
+    }
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (indexPath.section == 0) [self.navigationController pushViewController:[LogViewController new] animated:YES];
+}
+@end
