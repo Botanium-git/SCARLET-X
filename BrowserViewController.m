4f7a0aa27a233703d0c57d8f2d3e3c37303d94d6
Botanium-git <kbtfrdm@gmail.com>
2026-09-20 02:30:53 +0900

    Add settings diagnostics UI

--- BrowserViewController.m
+++ BrowserViewController.m
@@ -1,21 +1,22 @@
 #import "BrowserViewController.h"
+#import "SettingsViewController.h"
+#import "DiagnosticsStore.h"
 #import <WebKit/WebKit.h>
 
 @interface BrowserViewController () <WKNavigationDelegate, WKUIDelegate>
-@property (nonatomic, strong) WKWebView *webView;
-@property (nonatomic, strong) UIToolbar *toolbar;
-@property (nonatomic, strong) UILabel *statusLabel;
-@property (nonatomic, strong) NSURL *pendingURL;
+@property(nonatomic,strong) WKWebView *webView;
+@property(nonatomic,strong) UIToolbar *toolbar;
+@property(nonatomic,strong) NSURL *pendingURL;
 @end
 
 @implementation BrowserViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.systemBackgroundColor;
+    [[DiagnosticsStore shared] addEvent:@"App launched" detail:@"Browser view created" url:nil];
 
     WKWebViewConfiguration *config = [WKWebViewConfiguration new];
-    config.websiteDataStore = [WKWebsiteDataStore defaultDataStore];
+    config.websiteDataStore = WKWebsiteDataStore.defaultDataStore;
     config.allowsInlineMediaPlayback = YES;
     config.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
 
@@ -27,107 +28,67 @@
 
     UIBarButtonItem *back = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"chevron.backward"] style:UIBarButtonItemStylePlain target:self action:@selector(goBack)];
     UIBarButtonItem *forward = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"chevron.forward"] style:UIBarButtonItemStylePlain target:self action:@selector(goForward)];
-    UIBarButtonItem *reload = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(reloadPage)];
     UIBarButtonItem *home = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"house"] style:UIBarButtonItemStylePlain target:self action:@selector(goHome)];
+    UIBarButtonItem *reload = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(reloadPage)];
+    UIBarButtonItem *settings = [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"gearshape"] style:UIBarButtonItemStylePlain target:self action:@selector(openSettings)];
     UIBarButtonItem *flex = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-
-    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,0,170,30)];
-    self.statusLabel.font = [UIFont systemFontOfSize:11];
-    self.statusLabel.textAlignment = NSTextAlignmentCenter;
-    self.statusLabel.numberOfLines = 2;
-    self.statusLabel.text = @"ScarletX v0.1.0";
-    UIBarButtonItem *status = [[UIBarButtonItem alloc] initWithCustomView:self.statusLabel];
 
     self.toolbar = [UIToolbar new];
     self.toolbar.translatesAutoresizingMaskIntoConstraints = NO;
-    self.toolbar.items = @[back, flex, forward, flex, home, flex, reload, flex, status];
+    self.toolbar.items = @[back,flex,forward,flex,home,flex,reload,flex,settings];
 
-    [self.view addSubview:self.webView];
-    [self.view addSubview:self.toolbar];
+    [self.view addSubview:self.webView]; [self.view addSubview:self.toolbar];
     UILayoutGuide *safe = self.view.safeAreaLayoutGuide;
     [NSLayoutConstraint activateConstraints:@[
-        [self.webView.topAnchor constraintEqualToAnchor:safe.topAnchor],
-        [self.webView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
-        [self.webView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
-        [self.webView.bottomAnchor constraintEqualToAnchor:self.toolbar.topAnchor],
-        [self.toolbar.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
-        [self.toolbar.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
-        [self.toolbar.bottomAnchor constraintEqualToAnchor:safe.bottomAnchor]
+      [self.webView.topAnchor constraintEqualToAnchor:safe.topAnchor],
+      [self.webView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
+      [self.webView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
+      [self.webView.bottomAnchor constraintEqualToAnchor:self.toolbar.topAnchor],
+      [self.toolbar.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
+      [self.toolbar.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
+      [self.toolbar.bottomAnchor constraintEqualToAnchor:safe.bottomAnchor]
     ]];
 
-    if (self.pendingURL) {
-        NSURL *u = self.pendingURL; self.pendingURL = nil;
-        [self loadURL:u reason:@"pending"];
-    } else {
-        [self goHome];
-    }
+    if (self.pendingURL) { NSURL *u=self.pendingURL; self.pendingURL=nil; [self loadURL:u reason:@"pending"]; }
+    else [self goHome];
 }
-
-- (BOOL)isWebURL:(NSURL *)url {
-    NSString *scheme = url.scheme.lowercaseString;
-    return [scheme isEqualToString:@"https"] || [scheme isEqualToString:@"http"];
-}
-
+- (BOOL)isWebURL:(NSURL *)url { NSString *s=url.scheme.lowercaseString; return [s isEqual:@"https"]||[s isEqual:@"http"]; }
 - (NSURL *)unwrapScarletURL:(NSURL *)url {
-    if (![[url.scheme lowercaseString] isEqualToString:@"scarletx"]) return url;
-    NSURLComponents *c = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:NO];
-    for (NSURLQueryItem *item in c.queryItems) {
-        if ([item.name isEqualToString:@"url"] && item.value.length) {
-            NSURL *nested = [NSURL URLWithString:item.value];
-            if (nested) return nested;
-        }
-    }
+    if (![[url.scheme lowercaseString] isEqual:@"scarletx"]) return url;
+    NSURLComponents *c=[NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:NO];
+    for (NSURLQueryItem *i in c.queryItems) if ([i.name isEqual:@"url"]&&i.value.length) { NSURL *u=[NSURL URLWithString:i.value]; if(u)return u; }
     return [NSURL URLWithString:@"https://x.com/"];
 }
-
 - (void)openExternalURL:(NSURL *)url source:(NSString *)source {
-    if (!url) return;
-    NSURL *target = [self unwrapScarletURL:url];
-    NSLog(@"[ScarletX] incoming via %@: %@", source, target.absoluteString);
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (!self.isViewLoaded) self.pendingURL = target;
-        else [self loadURL:target reason:[NSString stringWithFormat:@"IN: %@", source]];
-    });
+    if(!url)return; NSURL *target=[self unwrapScarletURL:url];
+    [[DiagnosticsStore shared] addEvent:@"Received external URL" detail:source ?: @"" url:target];
+    dispatch_async(dispatch_get_main_queue(), ^{ if(!self.isViewLoaded)self.pendingURL=target; else [self loadURL:target reason:source]; });
 }
-
 - (void)loadURL:(NSURL *)url reason:(NSString *)reason {
-    if (![self isWebURL:url]) {
-        self.statusLabel.text = [NSString stringWithFormat:@"Unsupported\n%@", url.scheme ?: @"(none)"];
-        return;
-    }
-    self.statusLabel.text = reason;
+    if(![self isWebURL:url]) { [[DiagnosticsStore shared] addEvent:@"Unsupported URL" detail:url.scheme ?: @"" url:url]; return; }
+    [[DiagnosticsStore shared] addEvent:@"Loading URL" detail:reason ?: @"" url:url];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30]];
 }
-
-- (void)goHome { [self loadURL:[NSURL URLWithString:@"https://x.com/home"] reason:@"HOME"]; }
-- (void)goBack { if (self.webView.canGoBack) [self.webView goBack]; }
-- (void)goForward { if (self.webView.canGoForward) [self.webView goForward]; }
-- (void)reloadPage { [self.webView reload]; }
-
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
-    self.statusLabel.text = webView.URL.host ?: @"Loaded";
+- (void)goHome { [self loadURL:[NSURL URLWithString:@"https://x.com/home"] reason:@"Home"]; }
+- (void)goBack { if(self.webView.canGoBack)[self.webView goBack]; }
+- (void)goForward { if(self.webView.canGoForward)[self.webView goForward]; }
+- (void)reloadPage { [[DiagnosticsStore shared] addEvent:@"Reload" detail:@"" url:self.webView.URL]; [self.webView reload]; }
+- (void)openSettings {
+    UINavigationController *nav=[[UINavigationController alloc] initWithRootViewController:[SettingsViewController new]];
+    nav.modalPresentationStyle=UIModalPresentationPageSheet;
+    [self presentViewController:nav animated:YES completion:nil];
 }
-
-- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
-    self.statusLabel.text = [NSString stringWithFormat:@"ERR %ld\n%@", (long)error.code, error.localizedDescription];
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation { [[DiagnosticsStore shared] addEvent:@"Navigation started" detail:@"" url:webView.URL]; }
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation { [[DiagnosticsStore shared] addEvent:@"Navigation finished" detail:@"" url:webView.URL]; }
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error { [[DiagnosticsStore shared] addError:@"Provisional navigation failed" error:error url:webView.URL]; }
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error { [[DiagnosticsStore shared] addError:@"Navigation failed" error:error url:webView.URL]; }
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView { [[DiagnosticsStore shared] addEvent:@"Web content process terminated" detail:@"" url:webView.URL]; }
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)a decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    NSURL *url=a.request.URL; NSString *s=url.scheme.lowercaseString;
+    if([self isWebURL:url]||[s isEqual:@"about"]||[s isEqual:@"blob"]||[s isEqual:@"data"]) { decisionHandler(WKNavigationActionPolicyAllow); return; }
+    [[DiagnosticsStore shared] addEvent:@"Blocked scheme" detail:s ?: @"" url:url]; decisionHandler(WKNavigationActionPolicyCancel);
 }
-
-- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
-    NSURL *url = navigationAction.request.URL;
-    if ([self isWebURL:url] || [url.scheme.lowercaseString isEqualToString:@"about"] || [url.scheme.lowercaseString isEqualToString:@"blob"] || [url.scheme.lowercaseString isEqualToString:@"data"]) {
-        decisionHandler(WKNavigationActionPolicyAllow);
-        return;
-    }
-    NSLog(@"[ScarletX] blocked non-web navigation: %@", url.absoluteString);
-    self.statusLabel.text = [NSString stringWithFormat:@"Blocked\n%@", url.scheme ?: @"scheme"];
-    decisionHandler(WKNavigationActionPolicyCancel);
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)a windowFeatures:(WKWindowFeatures *)windowFeatures {
+    if(a.targetFrame==nil&&a.request.URL)[webView loadRequest:a.request]; return nil;
 }
-
-- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures {
-    if (navigationAction.targetFrame == nil && navigationAction.request.URL) {
-        [webView loadRequest:navigationAction.request];
-    }
-    return nil;
-}
-
 @end
