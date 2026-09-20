a3579d815c2cffe76f30e135eef865831c9c4a70
Botanium-git <kbtfrdm@gmail.com>
2026-09-20 18:17:18 +0900

    Add JSON diagnostic export and share sheet

--- LogViewController.m
+++ LogViewController.m
@@ -14,6 +14,7 @@
     self.formatter.dateFormat = @"MM/dd HH:mm:ss";
     self.navigationItem.rightBarButtonItems = @[
         [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(clearLog)],
+        [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"square.and.arrow.up"] style:UIBarButtonItemStylePlain target:self action:@selector(exportJSON)],
         [[UIBarButtonItem alloc] initWithImage:[UIImage systemImageNamed:@"doc.on.doc"] style:UIBarButtonItemStylePlain target:self action:@selector(copyAll)]
     ];
     [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"log"];
@@ -39,5 +40,37 @@
     return [rows componentsJoinedByString:@"\n\n"];
 }
 - (void)copyAll { UIPasteboard.generalPasteboard.string = [self allText]; }
+- (void)exportJSON {
+    NSArray *entries = DiagnosticsStore.shared.entries;
+    NSDictionary *export = @{
+        @"format": @"ScarletXDiagnostics",
+        @"version": @1,
+        @"exportedAt": @([[NSDate date] timeIntervalSince1970]),
+        @"appVersion": NSBundle.mainBundle.infoDictionary[@"CFBundleShortVersionString"] ?: @"",
+        @"build": NSBundle.mainBundle.infoDictionary[@"CFBundleVersion"] ?: @"",
+        @"entries": entries ?: @[]
+    };
+    NSError *error = nil;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:export options:NSJSONWritingPrettyPrinted error:&error];
+    if (!data) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Export Failed" message:error.localizedDescription ?: @"Could not create diagnostic JSON." preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+        return;
+    }
+    NSDateFormatter *nameFormatter = [NSDateFormatter new];
+    nameFormatter.dateFormat = @"yyyy-MM-dd_HHmmss";
+    NSString *name = [NSString stringWithFormat:@"ScarletX_Log_%@.json", [nameFormatter stringFromDate:[NSDate date]]];
+    NSURL *url = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:name]];
+    if (![data writeToURL:url options:NSDataWritingAtomic error:&error]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Export Failed" message:error.localizedDescription ?: @"Could not write diagnostic JSON." preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+        return;
+    }
+    UIActivityViewController *share = [[UIActivityViewController alloc] initWithActivityItems:@[url] applicationActivities:nil];
+    share.popoverPresentationController.barButtonItem = self.navigationItem.rightBarButtonItems.count > 1 ? self.navigationItem.rightBarButtonItems[1] : nil;
+    [self presentViewController:share animated:YES completion:nil];
+}
 - (void)clearLog { [DiagnosticsStore.shared clear]; self.items = @[]; [self.tableView reloadData]; }
 @end
