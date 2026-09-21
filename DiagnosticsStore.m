6944adc8a48ce0bf3d10856800e6de8420570f2d
Botanium-git <kbtfrdm@gmail.com>
2026-09-21 23:46:19 +0900

    Store diagnostic and error logs separately

--- DiagnosticsStore.m
+++ DiagnosticsStore.m
@@ -1,6 +1,7 @@
 #import "DiagnosticsStore.h"
 
-static NSString * const SXDiagnosticsKey = @"ScarletXDiagnostics";
+static NSString * const SXDiagnosticEventsKey = @"ScarletXDiagnosticEvents";
+static NSString * const SXErrorLogKey = @"ScarletXErrorLog";
 
 @implementation DiagnosticsStore
 + (instancetype)shared {
@@ -9,21 +10,23 @@ static NSString * const SXDiagnosticsKey = @"ScarletXDiagnostics";
     dispatch_once(&onceToken, ^{ store = [DiagnosticsStore new]; });
     return store;
 }
-- (void)saveEntry:(NSDictionary *)entry {
+- (void)saveEntry:(NSDictionary *)entry key:(NSString *)key {
     NSUserDefaults *d = NSUserDefaults.standardUserDefaults;
-    NSMutableArray *items = [[d arrayForKey:SXDiagnosticsKey] mutableCopy] ?: [NSMutableArray array];
+    NSMutableArray *items = [[d arrayForKey:key] mutableCopy] ?: [NSMutableArray array];
     [items insertObject:entry atIndex:0];
     if (items.count > 200) [items removeObjectsInRange:NSMakeRange(200, items.count - 200)];
-    [d setObject:items forKey:SXDiagnosticsKey];
+    [d setObject:items forKey:key];
 }
 - (void)addEvent:(NSString *)title detail:(NSString *)detail url:(NSURL *)url {
-    [self saveEntry:@{@"kind":@"event", @"title":title ?: @"Event", @"detail":detail ?: @"", @"url":url.absoluteString ?: @"", @"date":@([[NSDate date] timeIntervalSince1970])}];
+    [self saveEntry:@{@"kind":@"event", @"title":title ?: @"Event", @"detail":detail ?: @"", @"url":url.absoluteString ?: @"", @"date":@([[NSDate date] timeIntervalSince1970])} key:SXDiagnosticEventsKey];
 }
 - (void)addError:(NSString *)title error:(NSError *)error url:(NSURL *)url {
     NSString *detail = [NSString stringWithFormat:@"Domain: %@\nCode: %ld\nDescription: %@",
                         error.domain ?: @"", (long)error.code, error.localizedDescription ?: @""];
-    [self saveEntry:@{@"kind":@"error", @"title":title ?: @"Error", @"detail":detail, @"url":url.absoluteString ?: @"", @"date":@([[NSDate date] timeIntervalSince1970])}];
+    [self saveEntry:@{@"kind":@"error", @"title":title ?: @"Error", @"detail":detail, @"url":url.absoluteString ?: @"", @"date":@([[NSDate date] timeIntervalSince1970])} key:SXErrorLogKey];
 }
-- (NSArray<NSDictionary *> *)entries { return [NSUserDefaults.standardUserDefaults arrayForKey:SXDiagnosticsKey] ?: @[]; }
-- (void)clear { [NSUserDefaults.standardUserDefaults removeObjectForKey:SXDiagnosticsKey]; }
+- (NSArray<NSDictionary *> *)diagnosticEntries { return [NSUserDefaults.standardUserDefaults arrayForKey:SXDiagnosticEventsKey] ?: @[]; }
+- (NSArray<NSDictionary *> *)errorEntries { return [NSUserDefaults.standardUserDefaults arrayForKey:SXErrorLogKey] ?: @[]; }
+- (void)clearDiagnostics { [NSUserDefaults.standardUserDefaults removeObjectForKey:SXDiagnosticEventsKey]; }
+- (void)clearErrors { [NSUserDefaults.standardUserDefaults removeObjectForKey:SXErrorLogKey]; }
 @end
