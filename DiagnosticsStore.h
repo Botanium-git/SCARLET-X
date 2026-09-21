4b2014690a8846be5bc23254592c6d8534741a88
Botanium-git <kbtfrdm@gmail.com>
2026-09-21 23:46:14 +0900

    Separate diagnostic and error log APIs

--- DiagnosticsStore.h
+++ DiagnosticsStore.h
@@ -4,6 +4,8 @@
 + (instancetype)shared;
 - (void)addEvent:(NSString *)title detail:(NSString *)detail url:(NSURL *)url;
 - (void)addError:(NSString *)title error:(NSError *)error url:(NSURL *)url;
-- (NSArray<NSDictionary *> *)entries;
-- (void)clear;
+- (NSArray<NSDictionary *> *)diagnosticEntries;
+- (NSArray<NSDictionary *> *)errorEntries;
+- (void)clearDiagnostics;
+- (void)clearErrors;
 @end
