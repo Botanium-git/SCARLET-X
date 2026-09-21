9e71166cea3b5d11e2bd51c5e4f4515dbeb02758
Botanium-git <kbtfrdm@gmail.com>
2026-09-21 23:46:23 +0900

    Support separate log screens

--- LogViewController.h
+++ LogViewController.h
@@ -1,3 +1,10 @@
 #import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, SXLogKind) {
+    SXLogKindDiagnostics,
+    SXLogKindErrors
+};
+
 @interface LogViewController : UITableViewController
+- (instancetype)initWithLogKind:(SXLogKind)logKind;
 @end
