488b52f91a38365ae07ff2d71abb36192101add6
Botanium-git <kbtfrdm@gmail.com>
2026-09-23 05:41:37 +0900

    Rename script modules

---
+++ DiagnosticsScripts.m
@@ -0,0 +1,18 @@
+#import "DiagnosticsScripts.h"
+
+@implementation DiagnosticsScripts
++ (void)installFlagsInto:(WKUserContentController *)contentController {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    BOOL resourcesDiagnostics = [defaults boolForKey:@"ScarletXDiagResources"];
+    BOOL menuDiagnostics = [defaults boolForKey:@"ScarletXDiagMenu"];
+    BOOL requestDiagnostics = [defaults boolForKey:@"ScarletXDiagRequests"];
+    BOOL displayDiagnostics = [defaults boolForKey:@"ScarletXDiagDisplayDOM"];
+    BOOL headerStateDiagnostics = [defaults boolForKey:@"ScarletXDiagHeaderState"];
+    BOOL headerVisualDiagnostics = [defaults boolForKey:@"ScarletXDiagHeaderVisual"];
+    BOOL topNavTransformDiagnostics = [defaults boolForKey:@"ScarletXDiagTopNavTransform"];
+    BOOL paintProbeDiagnostics = [defaults boolForKey:@"ScarletXDiagPaintProbe"];
+    NSString *diagnosticFlagSource = [NSString stringWithFormat:@"window.__scarletXDiagnostics={resources:%@,menu:%@,requests:%@,display:%@,headerState:%@,headerVisual:%@,topNavTransform:%@,paintProbe:%@};", resourcesDiagnostics ? @"true" : @"false", menuDiagnostics ? @"true" : @"false", requestDiagnostics ? @"true" : @"false", displayDiagnostics ? @"true" : @"false", headerStateDiagnostics ? @"true" : @"false", headerVisualDiagnostics ? @"true" : @"false", topNavTransformDiagnostics ? @"true" : @"false", paintProbeDiagnostics ? @"true" : @"false"];
+    WKUserScript *diagnosticFlagScript = [[WKUserScript alloc] initWithSource:diagnosticFlagSource injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
+    [contentController addUserScript:diagnosticFlagScript];
+}
+@end
