c8a403fe9876b4291c07aa9f9d437466bfe33842
Botanium-git <kbtfrdm@gmail.com>
2026-09-23 05:18:47 +0900

    Limit navigation category to private helpers

--- BrowserViewController+Navigation.h
+++ BrowserViewController+Navigation.h
@@ -3,7 +3,6 @@
 @interface BrowserViewController (Navigation)
 - (BOOL)isWebURL:(NSURL *)url;
 - (NSURL *)unwrapScarletURL:(NSURL *)url;
-- (void)openExternalURL:(NSURL *)url source:(NSString *)source;
 - (void)loadURL:(NSURL *)url reason:(NSString *)reason;
 - (void)goHome;
 @end
