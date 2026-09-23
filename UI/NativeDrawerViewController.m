6837c0132b0b6ee8576ca8ea5a482acff40066c8
Botanium-git <kbtfrdm@gmail.com>
2026-09-23 15:38:33 +0900

    feat: align native drawer account header with X

--- UI/NativeDrawerViewController.m
+++ UI/NativeDrawerViewController.m
@@ -28,7 +28,7 @@
         @{@"title":@"ログアウト", @"icon":@"rectangle.portrait.and.arrow.right"}
     ];
 
-    self.dimmingView = [UIView new]; self.dimmingView.backgroundColor=[UIColor colorWithWhite:0 alpha:.35]; self.dimmingView.alpha=0; self.dimmingView.translatesAutoresizingMaskIntoConstraints=NO; [self.view addSubview:self.dimmingView]; [self.dimmingView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(backgroundTapped:)]];
+    self.dimmingView=[UIView new]; self.dimmingView.backgroundColor=[UIColor colorWithWhite:0 alpha:.35]; self.dimmingView.alpha=0; self.dimmingView.translatesAutoresizingMaskIntoConstraints=NO; [self.view addSubview:self.dimmingView]; [self.dimmingView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(backgroundTapped:)]];
     self.panelView=[UIView new]; self.panelView.backgroundColor=UIColor.systemBackgroundColor; self.panelView.translatesAutoresizingMaskIntoConstraints=NO; [self.view addSubview:self.panelView];
     self.tableView=[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain]; self.tableView.backgroundColor=UIColor.systemBackgroundColor; self.tableView.separatorStyle=UITableViewCellSeparatorStyleNone; self.tableView.dataSource=self; self.tableView.delegate=self; self.tableView.rowHeight=54; self.tableView.translatesAutoresizingMaskIntoConstraints=NO; [self.panelView addSubview:self.tableView];
     self.tableView.tableHeaderView=[self buildProfileHeader];
@@ -38,14 +38,32 @@
     UISwipeGestureRecognizer *swipe=[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipedClosed:)]; swipe.direction=UISwipeGestureRecognizerDirectionLeft; [self.panelView addGestureRecognizer:swipe];
 }
 
+- (void)loadImageURLString:(NSString *)urlString into:(UIImageView *)imageView {
+    if(![urlString isKindOfClass:NSString.class]||urlString.length==0)return;
+    NSURL *url=[NSURL URLWithString:urlString]; if(!url)return;
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data,NSURLResponse *response,NSError *error){ if(!data)return; UIImage *image=[UIImage imageWithData:data]; if(!image)return; dispatch_async(dispatch_get_main_queue(),^{ imageView.image=image; }); }] resume];
+}
+
 - (UIView *)buildProfileHeader {
-    UIView *header=[[UIView alloc] initWithFrame:CGRectMake(0,0,340,142)];
-    UIImageView *avatar=[[UIImageView alloc] initWithFrame:CGRectMake(16,12,44,44)]; avatar.layer.cornerRadius=22; avatar.clipsToBounds=YES; avatar.backgroundColor=UIColor.secondarySystemBackgroundColor; avatar.image=[UIImage systemImageNamed:@"person.crop.circle.fill"]; [header addSubview:avatar];
-    NSString *avatarURL=[self.profileData[@"avatarURL"] isKindOfClass:NSString.class]?self.profileData[@"avatarURL"]:nil;
-    if(avatarURL.length){ NSURL *u=[NSURL URLWithString:avatarURL]; if(u) NSURLSession.sharedSession ? [NSURLSession.sharedSession dataTaskWithURL:u completionHandler:^(NSData *data,NSURLResponse *response,NSError *error){ if(!data)return; UIImage *img=[UIImage imageWithData:data]; if(!img)return; dispatch_async(dispatch_get_main_queue(),^{ avatar.image=img; }); }] .resume : (void)0; }
-    UILabel *name=[[UILabel alloc] initWithFrame:CGRectMake(16,64,300,24)]; name.font=[UIFont systemFontOfSize:17 weight:UIFontWeightBold]; name.text=[self.profileData[@"name"] isKindOfClass:NSString.class]?self.profileData[@"name"]:@""; [header addSubview:name];
-    UILabel *handle=[[UILabel alloc] initWithFrame:CGRectMake(16,87,300,20)]; handle.font=[UIFont systemFontOfSize:15]; handle.textColor=UIColor.secondaryLabelColor; handle.text=[self.profileData[@"handle"] isKindOfClass:NSString.class]?self.profileData[@"handle"]:@""; [header addSubview:handle];
-    UILabel *counts=[[UILabel alloc] initWithFrame:CGRectMake(16,112,310,22)]; counts.font=[UIFont systemFontOfSize:14]; counts.textColor=UIColor.secondaryLabelColor; NSString *following=[self.profileData[@"following"] isKindOfClass:NSString.class]?self.profileData[@"following"]:@""; NSString *followers=[self.profileData[@"followers"] isKindOfClass:NSString.class]?self.profileData[@"followers"]:@""; counts.text=[NSString stringWithFormat:@"%@ フォロー中    %@ フォロワー",following,followers]; [header addSubview:counts];
+    UIView *header=[[UIView alloc] initWithFrame:CGRectMake(0,0,340,154)];
+
+    UIImageView *avatar=[[UIImageView alloc] initWithFrame:CGRectMake(16,10,44,44)];
+    avatar.layer.cornerRadius=22; avatar.clipsToBounds=YES; avatar.contentMode=UIViewContentModeScaleAspectFill; avatar.backgroundColor=UIColor.secondarySystemBackgroundColor; avatar.image=[UIImage systemImageNamed:@"person.crop.circle.fill"]; [header addSubview:avatar];
+    [self loadImageURLString:self.profileData[@"avatarURL"] into:avatar];
+
+    NSArray *accounts=[self.profileData[@"accounts"] isKindOfClass:NSArray.class]?self.profileData[@"accounts"]:@[];
+    CGFloat x=284.0;
+    for(NSDictionary *account in [accounts reverseObjectEnumerator]){
+        NSString *url=[account[@"avatarURL"] isKindOfClass:NSString.class]?account[@"avatarURL"]:@"";
+        if(url.length==0||[url isEqual:self.profileData[@"avatarURL"]])continue;
+        UIImageView *other=[[UIImageView alloc] initWithFrame:CGRectMake(x,14,36,36)]; other.layer.cornerRadius=18; other.clipsToBounds=YES; other.contentMode=UIViewContentModeScaleAspectFill; other.backgroundColor=UIColor.secondarySystemBackgroundColor; other.image=[UIImage systemImageNamed:@"person.crop.circle.fill"]; [header addSubview:other]; [self loadImageURLString:url into:other]; x-=44; if(x<152)break;
+    }
+
+    UILabel *handle=[[UILabel alloc] initWithFrame:CGRectMake(16,60,308,21)]; handle.font=[UIFont systemFontOfSize:15]; handle.textColor=UIColor.secondaryLabelColor; handle.text=[self.profileData[@"handle"] isKindOfClass:NSString.class]?self.profileData[@"handle"]:@""; [header addSubview:handle];
+
+    UILabel *name=[[UILabel alloc] initWithFrame:CGRectMake(16,84,308,24)]; name.font=[UIFont systemFontOfSize:17 weight:UIFontWeightBold]; name.text=[self.profileData[@"name"] isKindOfClass:NSString.class]?self.profileData[@"name"]:@""; [header addSubview:name];
+
+    UILabel *counts=[[UILabel alloc] initWithFrame:CGRectMake(16,116,310,22)]; counts.font=[UIFont systemFontOfSize:14]; counts.textColor=UIColor.secondaryLabelColor; NSString *following=[self.profileData[@"following"] isKindOfClass:NSString.class]?self.profileData[@"following"]:@""; NSString *followers=[self.profileData[@"followers"] isKindOfClass:NSString.class]?self.profileData[@"followers"]:@""; counts.text=[NSString stringWithFormat:@"%@ フォロー中    %@ フォロワー",following,followers]; [header addSubview:counts];
     return header;
 }
 
