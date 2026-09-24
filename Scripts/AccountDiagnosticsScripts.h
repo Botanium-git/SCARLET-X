eee4b0f0fd6c49ba507104941727750184bbc98e
Botanium-git <kbtfrdm@gmail.com>
2026-09-25 01:54:45 +0900

    Remove superseded standalone account diagnostics header

--- Scripts/AccountDiagnosticsScripts.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <WebKit/WebKit.h>
-
-@interface AccountDiagnosticsScripts : NSObject
-+ (void)installInto:(WKUserContentController *)contentController;
-@end
